109e64506693f2d156589404f4c6d03560f2e296
cswilly40 <cswilly40@gmail.com>
2021-03-08 23:18:33 +0800

    類別屬性、方法抽取、浮動文字顯示

--- app/src/main/java/com/cswilly/bmi_j/MainActivity.java
+++ app/src/main/java/com/cswilly/bmi_j/MainActivity.java
@@ -6,21 +6,34 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews() {
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+        result = findViewById(R.id.result);
     }
 
     public void bmi(View view) {
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight = findViewById(R.id.ed_height);
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight / (height * height);
         Log.d("MainActivity", "BMI: " + bmi);
+        Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
+        result.setText("Your BMI is " + bmi);
     }
 }
