ab6ac2b59815b68a0f00ea7c7c041433d1645cde
cswilly40 <cswilly40@gmail.com>
2021-03-10 23:53:47 +0800

    對話框元件與匿名類別實務

--- app/src/main/java/com/cswilly/bmi_j/MainActivity.java
+++ app/src/main/java/com/cswilly/bmi_j/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cswilly.bmi_j;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 //        findViews();
+
+        binding.help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Help")
+                        .setMessage("Body mass index (BMI) is a value derived from the mass (weight) and height of a person.")
+                        .setPositiveButton("OK",null)
+                        .show();
+            }
+        });
     }
 
     /*private void findViews() {
@@ -36,8 +49,24 @@ public class MainActivity extends AppCompatActivity {
         float weight = Float.parseFloat(binding.weight.getText().toString());
         float height = Float.parseFloat(binding.height.getText().toString());
         float bmi = weight / (height * height);
+
         Log.d("MainActivity", "BMI: " + bmi);
+
         Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
+
         binding.result.setText("Your BMI is " + bmi);
+
+        new AlertDialog.Builder(this)
+                .setTitle("BMI")
+                .setMessage("Your BMI is " + bmi)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        binding.weight.setText("");
+                        binding.height.setText("");
+                        binding.result.setText("");
+                    }
+                })
+                .show();
     }
 }
